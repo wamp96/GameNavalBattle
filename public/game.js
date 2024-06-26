9842ed5223abfa4b647fc88084fec9b4665c30aa
wamp96 <wamoreno57@gmail.com>
2024-06-26 00:06:28 -0500

    Actualizacion="Se finaliza actualizacion de funciones necesarias  para el manejo de la tabla y el ingreso de sesion de usuario, adicional validaciones"

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-const socket =io();
+const socket = io();
 
 const startScreen = document.getElementById('start-screen');
 const lobby = document.getElementById('lobby');
@@ -53,5 +53,85 @@ socket.on('chatMessage',(message)=>{
     messages.scrollTop = messages.scrollHeight;
 });
 
+chatInput.addEventListener('keypress',(event)=>{
+    if(event.key === 'Enter' &&chatInput.value){
+        socket.emit('sendMessage', chatInput.value);
+        chatInput.value = '';
+    }
+});
 
 
+function setupBoards(){
+    createBoard(playerBoard,'player');
+    createBoard(opponentBoard,'opponent');
+}
+
+function createBoard(boardElement, type){
+    for(let i = 0; i<10;i++){
+        for(let j = 0; j<10;j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            if(type === 'opponent'){
+                cell.addEventListener('click',()=>{
+                    socket.emit('makeMove',{row:i,col:j});
+                });
+            }
+            boardElement.appendChild(cell);
+        }
+    }
+}
+
+
+socket.on('updateBoard',(board)=>{
+    updateBoard(playerBoard,board.player);
+    updateBoard(opponentBoard,board.opponent);
+    updateTurnIndicator(board.turn);
+    updateMoveHistory(board.history);
+});
+
+
+socket.on('gameOver',(result)=>{
+    resultMessage.style.display = 'block';
+    resultMessage.textContent = result.winner === socket.id ? '!GANASTE¡':'!PERDIO¡';
+    turnIndicator.style.display = 'none';
+
+    if(result.winner === socket.id){
+        winSound.play();
+    }else{
+        loseSound.play();
+    }
+
+});
+
+
+function updateBoard(boardElement,boardData){
+    const cells = boardElement.getElementByClassName('cell');
+    for(let i = 0; i < cells.length; i++){
+        const cell = cells[i];
+        const row = Math.floor(i/10);
+        const col = i % 10;
+        cell.className = 'cell';
+        if(boardData[row][col]==='ship'){
+            cell.classList.add('ship');
+        }else if(boardData[row][col]==='hit'){
+            cell.classList.add('hit');
+            hitSound.play();
+        }else if(boardData[row][col]==='miss'){
+            cell.classList.add('miss');
+            missSound.play();
+        }
+    }
+}
+
+function updateTurnIndicator(turn){
+    turnIndicator.textContent = turn === socket.id ? 'Es tu turno ' : 'Turno del oponente'
+}
+
+function updateMoveHistory(history){
+    historyList.innerHTML = '';
+    history.forEach(move =>{
+        const listItem = document.createElement('li');
+        listItem.textContent = `${move.player === socket.id ? 'Tu': 'Oponente'} ataco(${move.row},${move.col})`;
+        historyList.appendChild(listItem);
+    });
+}
